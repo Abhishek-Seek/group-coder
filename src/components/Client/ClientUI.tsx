cce6cbf0ddf6e8cd47e58f8f9a5d4cada3da48ad
softpro24 <darkworld1914@gmail.com>
2024-05-18 13:03:16 +0530

    setup files

---
+++ src/components/Client/ClientUI.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClientUI = () => {
+  return (
+    <div>
+      this is user interface
+    </div>
+  )
+}
+
+export default ClientUI
---
+++ src/components/Login/Signin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+       this is signin pagw
+    </div>
+  )
+}
+
+export default Signin
